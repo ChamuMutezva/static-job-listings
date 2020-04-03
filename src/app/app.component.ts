9cccdcc97f99e18f94014517473af3cc36f88321
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-04-03 21:36:47 +0200

    linking an image in the html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'static-job-listings';
+  showImage: boolean = false;
+
+  toggleImage() : void {
+    this.showImage = !this.showImage;
+  }
 }
